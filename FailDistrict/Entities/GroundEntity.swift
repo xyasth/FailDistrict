79bb1627c399f4d29a1ae527a3df9aa5ac7a482e
Hansel Meinhard <hansel.meinhard04@gmail.com>
2026-05-18 15:14:45 +0700

    feat: remove ground color

--- FailDistrict/Entities/GroundEntity.swift
+++ FailDistrict/Entities/GroundEntity.swift
@@ -6,7 +6,7 @@ class GroundEntity: GKEntity {
     var spriteNode: SKSpriteNode
     
     init(size: CGSize, position: CGPoint) {
-        spriteNode = SKSpriteNode(color: NSColor.green.withAlphaComponent(0.4), size: size)
+        spriteNode = SKSpriteNode(color: .clear, size: size)
         spriteNode.position = position
         
         spriteNode.physicsBody = SKPhysicsBody(rectangleOf: size)
